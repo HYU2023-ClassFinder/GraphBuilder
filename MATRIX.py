98619c893f6f1bf34f2a0f5c2a3677c208e3aab6
박정우 <2018008386@hanyang.ac.kr>
2023-11-13 12:53:59 +0900

    add comments

--- MATRIX.py
+++ MATRIX.py
@@ -25,14 +25,18 @@ triples = []
 subEs = []
 subTs = []
 
+# trivial한 entity 제거 -> 무의미한 hub가 생기는 것을 방지합니다.
 trivial = ["Programming_language", "computer_programming", "Computer_programming", "Computer_science", "Computer", "Image", "Integer", "academic_discipline", "field_of_study", "Personal_computer", "Application_software", "Robot"]
 # trivial = []
 
+# entity와 triple list에 내용들 넣기
 for line in rdr:
     if(len(line[0].split('\t')) == 1):
         continue
 
     twoEntities = line[0].split('\t')
+
+    # triple tuple의 세 번째 원소는 score기반 edge의 weight입니다. weight = 1 - score
     try:
         if(twoEntities[0].upper() not in trivial and twoEntities[1].upper() not in trivial):
             entities.append(mappedTagv3[twoEntities[0].upper()])
@@ -41,6 +45,7 @@ for line in rdr:
     except KeyError:
         continue
 
+    # score가 0.9 초과인 triple만 따로 모아 subE와 subT를 만들어봤습니다. -> 쓰지는 않았음
     try:
         if(float(line[1]) > 0.9 and twoEntities[0].upper() not in trivial and twoEntities[1].upper() not in trivial):
             subEs.append(mappedTagv3[twoEntities[0].upper()])
@@ -50,9 +55,11 @@ for line in rdr:
         continue
 f.close()
 
+# 중복 제거
 entities = list(set(entities))
 subEs = list(set(subEs))
 
+# 그래프 생성하고 node set, edge set 넣습니다.
 G = nx.DiGraph()
 subG = nx.DiGraph()
 
@@ -91,11 +98,15 @@ cur = conn.cursor()
 #             where course.id = review.lectureId group by lectureId), course
 #         where rownum <= 52 and star >= 4.0 and course.id = lID
 #         '''
+
+# 강의 데이터 가져올 쿼리
 query = '''
 select course.name, tag.tagDetail, course.star, course.regCount
 from course, tag
 where course.id = tag.lectureId
 '''
+
+# minMax normalization할 값들 가져올 쿼리
 minMaxQuery = '''
 select min(star), max(star), min(regCount), max(regCount)
 from course, tag
@@ -112,6 +123,7 @@ maxStar = minMax[0][1]
 minRegs = minMax[0][2]
 maxRegs = minMax[0][3]
 
+# 그래프 G 안의 a에서 출발해서 b로 도착하는 path를 최대 3개까지 찾습니다.
 try:
     pathList = []
     X = nx.shortest_simple_paths(G, a, b)
@@ -120,10 +132,10 @@ try:
         pathList.append(path)
         if counter == k-1:
             break
-except NetworkXNoPath:
+except NetworkXNoPath: # path가 없을 때
     print("No path between", a, "and", b)
     pathList = []
-except nx.exception.NodeNotFound:
+except nx.exception.NodeNotFound: # a 또는 b가 없을 때
     print("No", a, "or", b)
     pathList = []
 
@@ -154,9 +166,11 @@ neighborhoods2 = [[] for _ in list(range(len(pathList)))]
 union = [[] for _ in list(range(len(pathList)))]
 startAndEnd = []
 
+# 그래프로 시각화하고, 각 path에 대한 추천 강의 목록을 가져옵니다.
 for k in list(range(len(pathList)-1, -1, -1)):
     recommedingCourses = [[] for _ in list(range(len(pathList[k])))]
 
+# 별점, 수강생을 minMax normalization
     print(k, list(map(tempFunc, pathList[k])))
     for i in list(range(len(pathList[k]))):
         for recommendingCandidate in recommendingCandidates:
@@ -165,7 +179,7 @@ for k in list(range(len(pathList)-1, -1, -1)):
                     recommedingCourses[i].append((recommendingCandidate[0], (recommendingCandidate[2]-minStar)/(maxStar-minStar), (recommendingCandidate[3]-minRegs)/(maxRegs-minRegs)))
             except KeyError:
                 continue
-
+# normalize한 점수의 평균과 함께, 강의 출력
     print("-----------------------------")
     for i in list(range(len(pathList[k]))):
         print("for ", tempFunc(pathList[k][i]))
@@ -180,6 +194,7 @@ for k in list(range(len(pathList)-1, -1, -1)):
                 print(recommedingCourses[i][j][0], (recommedingCourses[i][j][1]+recommedingCourses[i][j][2])/2)
         print("-----------------------------")
 
+# 그래프 그리기 위한 변수 선언
     codePath[k] = pathList[k].copy()
     _path[k] = list(map(tempFunc, pathList[k]))
     mains[k] = G.subgraph(codePath[k])
@@ -203,6 +218,7 @@ startAndEnd = nx.relabel_nodes(startAndEnd, mappedTagv2_codeToTag)
 
 # print("is it same?", (_path[2][0] == _path[1][0]))
 
+# 그래프를 예쁘게 그리기 위해, 위아래로 조금씩 노드의 위치를 변경합니다.
 for k in list(range(len(pathList)-1, -1, -1)):
     if(k == 2):
         for i in list(range(len(_path[k]))):
@@ -216,9 +232,15 @@ for k in list(range(len(pathList)-1, -1, -1)):
         for i in list(range(len(_path[k]))):
             # print(0, _path[k][i])
             pos[_path[k][i]] = [i-len(_path[k])/2, (-0.3)**i]
+
+# 시작 노드와 끝 노드의 위치는 고정합니다.
 pos[_path[0][0]] = [-len(_path[0])/2, 0]
 pos[_path[0][-1]] = [+len(_path[0])/2, 0]
 
+# 그래프를 그립니다.
+# 파랑, 초록, 빨강 순으로 path를 보이게 하기 위해, 빨강 path를 먼저 그리고, 초록, 파랑 순으로 그려서
+# 파랑이 가장 위에 보이게 합니다.
+# neighbors는 각 path들에 속한 노드의 neighbor 집합입니다.
 for k in list(range(len(pathList)-1, -1, -1)):
     if(k == 2):
         nx.draw_networkx_nodes(neighbors[k], pos=pos, node_size=75, node_color='grey')
